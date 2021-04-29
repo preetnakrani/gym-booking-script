fe01977e560003ec72f1313eed79b400a3f95ce3
preetnakrnai <preetnakrani@gmail.com>
2021-04-29 14:15:23 -0700

    new stuff

--- booker.js
+++ booker.js
@@ -3,7 +3,7 @@
  */
  const puppeteer = require('puppeteer');
 
-exports.scheduledEventLoggerHandler = async (event, context) => {
+exports.bookingHandler = async (event, context) => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.log(JSON.stringify(event));
